a997f76406324e666ff2acb271d6c5b82116c3b6
FaisalMuhammed <faisal707974@gmail.com>
2022-08-25 20:08:07 +0530

    api reconnected instead of json

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,18 +5,14 @@ import axios from 'axios'
 import Header from '../../components/header'
 import Category from '../../components/category'
 
-import jsondata from '../../data.json'
-
 export default function Index() {
 
     const [data, setData] = useState()
 
     useEffect(() => {
         async function getData() {
-            // const response = await axios.get('https://run.mocky.io/v3/a67edc87-49c7-4822-9cb4-e2ef94cb3099')
-            // setData(response.data)
-
-            setData(jsondata)
+            const response = await axios.get('https://run.mocky.io/v3/a67edc87-49c7-4822-9cb4-e2ef94cb3099')
+            setData(response.data)
         }
         getData()
     })
